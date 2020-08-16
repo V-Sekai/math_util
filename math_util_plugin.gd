9597fff7b0d665c0b0bfb28497017182e073e7a5
Saracen <SaracenOne@gmail.com>
2020-08-16 22:14:36 +0100

    Add printed messages for initialisation and destruction.

--- math_util_plugin.gd
+++ math_util_plugin.gd
@@ -1,11 +1,24 @@
 extends EditorPlugin
 tool
 
+
+func _init() -> void:
+	print("Initialising MathUtil plugin")
+
+
+func _notification(p_notification: int):
+	match p_notification:
+		NOTIFICATION_PREDELETE:
+			print("Destroying MathUtil plugin")
+
+
 func get_name() -> String: 
 	return "MathUtil"
 
+
 func _enter_tree() -> void:
 	add_autoload_singleton("GodotMathExtension", "res://addons/math_util/math_funcs.gd")
+
 
 func _exit_tree() -> void:
 	remove_autoload_singleton("GodotMathExtension")
